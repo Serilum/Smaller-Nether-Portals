e79020c52327cc846ed30a937729cf836a202167
Rick South <rick@natamus.com>
2024-04-28 18:57:42 +0200

    (v3.7) (1.20.5 Neo) Updated to work with NeoForge's TickEvent changes in 20.5.14.

--- NeoForge/src/main/java/com/natamus/smallernetherportals/neoforge/events/NeoForgePortalEvent.java
+++ NeoForge/src/main/java/com/natamus/smallernetherportals/neoforge/events/NeoForgePortalEvent.java
@@ -5,12 +5,11 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.TickEvent;
-import net.neoforged.neoforge.event.TickEvent.PlayerTickEvent;
-import net.neoforged.neoforge.event.entity.player.PlayerEvent.PlayerChangedDimensionEvent;
-import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent.PlayerChangedDimensionEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 
 @EventBusSubscriber
 public class NeoForgePortalEvent {
@@ -31,10 +30,10 @@ public class NeoForgePortalEvent {
 	}
 	
 	@SubscribeEvent
-	public static void onPlayerTick(PlayerTickEvent e) {
-		Player player = e.player;
+	public static void onPlayerTick(PlayerTickEvent.Pre e) {
+		Player player = e.getEntity();
 		Level level = player.level();
-		if (level.isClientSide || !e.phase.equals(TickEvent.Phase.START)) {
+		if (level.isClientSide) {
 			return;
 		}
 		
