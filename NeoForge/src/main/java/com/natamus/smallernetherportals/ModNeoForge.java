820767c798282c59876c7968942d956af47da70c
Rick South <rick@natamus.com>
2024-07-28 07:14:48 +0200

    (v3.8) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/smallernetherportals/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/smallernetherportals/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.smallernetherportals;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.smallernetherportals.neoforge.events.NeoForgePortalEvent;
 import com.natamus.smallernetherportals.util.Reference;
 import net.neoforged.neoforge.common.NeoForge;
@@ -12,6 +13,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
