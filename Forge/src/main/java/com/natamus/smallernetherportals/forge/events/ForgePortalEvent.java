b27a685f9dab73efe35a8d220597b116a45211db
Rick South <rick@natamus.com>
2025-03-28 00:29:45 +0100

    Smaller Nether Portals source code for 1.21.5.

---
+++ Forge/src/main/java/com/natamus/smallernetherportals/forge/events/ForgePortalEvent.java
@@ -0,0 +1,41 @@
+package com.natamus.smallernetherportals.forge.events;
+
+import com.natamus.smallernetherportals.events.PortalEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.TickEvent.PlayerTickEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent.PlayerChangedDimensionEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgePortalEvent {
+	@SubscribeEvent
+	public static void onClick(PlayerInteractEvent.RightClickBlock e) {
+		PortalEvent.onClick(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec());
+	}
+	
+	@SubscribeEvent
+	public static void onDimensionChange(PlayerChangedDimensionEvent e) {
+		Player player = e.getEntity();
+		Level level = player.level();
+		if (level.isClientSide) {
+			return;
+		}
+		
+		PortalEvent.onDimensionChange((ServerLevel)level, (ServerPlayer)player);
+	}
+	
+	@SubscribeEvent
+	public static void onPlayerTick(PlayerTickEvent e) {
+		Player player = e.player;
+		Level level = player.level();
+		if (level.isClientSide || !e.phase.equals(TickEvent.Phase.START)) {
+			return;
+		}
+		
+		PortalEvent.onPlayerTick((ServerLevel)level, (ServerPlayer)player);
+	}
+}
